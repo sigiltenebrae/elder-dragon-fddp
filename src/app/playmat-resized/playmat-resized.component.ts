f9f78548a580682592adb3bbfc92cdc5f63449d6
Christian Kobb <ck6202@rit.edu>
2022-09-12 04:26:15 -0400

    cascade fixed

--- src/app/playmat-resized/playmat-resized.component.ts
+++ src/app/playmat-resized/playmat-resized.component.ts
@@ -655,8 +655,9 @@ export class PlaymatResizedComponent implements OnInit {
     while(true) {
       if (this.user.deck.cards.length > 0) {
         let cur_card = this.user.deck.cards[0];
+        this.selectCard(cur_card, this.user.deck.cards);
         this.sendCardToZone(cur_card, this.user.deck.cards, 'temp_zone');
-        if (cur_card.cmc) {
+        if (cur_card.cmc != null) {
           if (cur_card.cmc < cmc) {
             if (cur_card.cmc > 0) {
               break;
