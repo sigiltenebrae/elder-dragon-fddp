7b920ca90abd038b11237a7e1ceaed7546c59023
Christian Kobb <ck6202@rit.edu>
2023-02-13 11:29:48 -0500

    shifted checks and promises to make dashboard load faster

--- src/app/deck-manager/deck-manager.component.ts
+++ src/app/deck-manager/deck-manager.component.ts
@@ -18,11 +18,9 @@ export class DeckManagerComponent implements OnInit {
   loading_others = false;
   loaded_others = false;
   decks_others: any = {};
+  show_others: boolean = false;
 
   temp = false;
-
-  ban_types: any[] = [];
-  ban_list: any[] = [];
 
   current_errors = [];
 
@@ -37,37 +35,17 @@ export class DeckManagerComponent implements OnInit {
     else {
       this.user = this.tokenStorage.getUser();
       if(this.user && this.user.id) {
-        this.fddp_data.getDecksBasic(this.user.id).then((decks: any) => {
-          let temp_decks = decks;
-          let deck_promises: any[] = [];
-          temp_decks.forEach((deck: any) => {
-            deck_promises.push(this.getDeckData(deck.id));
-          });
-          Promise.all(deck_promises).then(() => {
-
-            this.fddp_data.getBanList().then((banned_cards: any) => {
-              this.fddp_data.getBanTypes().then((ban_types) => {
-                this.ban_types = ban_types;
-                this.ban_list = [[], [], [], []];
-                banned_cards.forEach((card: any) => {
-                  this.ban_list[card.ban_type - 1].push(card);
-                });
-
-                let legality_promises = [];
-                for (let deck of this.decks) {
-                  deck.hovered = false;
-                  legality_promises.push(this.getDeckLegality(deck));
-                  Promise.all(legality_promises).then(() => {
-                    console.log('Decks loaded for user: ' + this.user.username);
-                  });
-                }
-                this.fddp_data.getUsers().then((user_list: any) => {
-                  this.users = user_list;
-                  this.loading = false;
-                });
-
+        this.fddp_data.getUsers().then((user_list: any) => {
+          this.users = user_list;
+          this.fddp_data.getDecksBasic(this.user.id).then((decks: any) => {
+            this.decks = decks;
+            this.loading = false;
+            for (let deck of this.decks) {
+              deck.hovered = false;
+              this.fddp_data.getDeckLegality(deck.id).then((issues) => {
+                deck.issues = issues;
               });
-            });
+            }
           });
         });
       }
@@ -81,133 +59,23 @@ export class DeckManagerComponent implements OnInit {
         this.decks_others[other.id] = [];
       }
     }
-
     this.fddp_data.getDecksBasic().then((decks: any) => {
-      let temp_decks = decks;
-      let deck_promises: any[] = [];
-      temp_decks.forEach((deck: any) => {
+      decks.forEach((deck: any) => {
         if (deck.owner !== this.user.id) {
-          deck_promises.push(this.getDeckData(deck.id));
-        }
-      });
-      Promise.all(deck_promises).then(() => {
-        let legality_promises = [];
-        for (let key in this.decks_others) {
-          for (let other_deck of this.decks_others[key]) {
-            other_deck.hovered = false;
-            legality_promises.push(this.getDeckLegality(other_deck));
-          }
-          Promise.all(legality_promises).then(() => {
-            this.loading_others = false;
-            this.loaded_others = true;
-          });
-        }
-      })
-    })
-  }
-
-  /**
-   * Get the necessary data to display the deck.
-   * @param deckid
-   */
-  getDeckData(deckid: number): Promise<void> {
-    return new Promise<void>((resolve) => {
-      this.fddp_data.getDeckForPlay(deckid).then((deck) => {
-        deck.commander = [];
-        deck.cards.forEach((card: any) => {
-          if (card.iscommander) {
-            deck.commander.push(card);
-          }
-        });
-        deck.commander.forEach((card: any) => {
-          deck.cards.splice(deck.cards.indexOf(card), 1);
-        });
-        deck.colors = this.getDeckColors(deck);
-        if (deck.owner == this.user.id) {
-          this.decks.push(deck);
-        }
-        else {
           this.decks_others[deck.owner].push(deck);
         }
-        resolve();
-      })
-    })
-  }
+      });
+      this.loading_others = false;
+      this.loaded_others = true;
 
-  /**
-   * Get the color identity for a deck
-   * @param deck
-   */
-  getDeckColors(deck: any) {
-    let colors: any = null;
-    for (let commander of deck.commander) {
-      if (commander.color_identity) {
-        if (colors == null) {
-          colors = [];
-        }
-        for (let mana of commander.color_identity) {
-          if (mana === 'W' || mana === 'U' || mana === 'B' || mana === 'R' || mana === 'G'){
-            colors.push(mana);
-          }
+      for (let key in this.decks_others) {
+        for (let other_deck of this.decks_others[key]) {
+          other_deck.hovered = false;
+          this.fddp_data.getDeckLegality(other_deck.id).then((issues) => {
+            other_deck.issues = issues;
+          });
         }
       }
-    }
-    return colors;
-  }
-
-  getBanId(ban_type: string){
-    for (let type of this.ban_types) {
-      if (ban_type === type.type) {
-        return type.id;
-      }
-    }
-  }
-
-  getDeckLegality(deck: any) {
-    return new Promise<void>((resolve) => {
-      if (deck.cards == null || deck.cards.length == 0) {
-        deck.legality = 'unknown';
-        deck.issues = [];
-      }
-      else {
-        let banned_cards = [];
-        deck.cards.forEach((card: any) => {
-          for (let banned_card of this.ban_list[this.getBanId("banned") - 1]) {
-            if (card.name === banned_card.name) {
-              banned_cards.push({name: card.name, gatherer: card.gatherer});
-              break;
-            }
-          }
-          if (!card.legality || card.cheapest > 25) {
-            let card_allowed = false;
-            if (card.iscommander) {
-              for (let unbanned_commander of this.ban_list[this.getBanId("allowed as commander") - 1]) {
-                if (card.name === unbanned_commander.name) {
-                  card_allowed = true;
-                  break;
-                }
-              }
-
-            }
-            if (!card_allowed) {
-              for (let unbanned_card of this.ban_list[this.getBanId("unbanned") - 1]) {
-                if (card.name === unbanned_card.name) {
-                  card_allowed = true;
-                  break;
-                }
-              }
-            }
-            if (!card_allowed) {
-              banned_cards.push({name: card.name, gatherer: card.gatherer});
-            }
-          }
-        });
-        deck.legality = banned_cards.length > 0 ? "illegal": "legal";
-        deck.issues = banned_cards;
-      }
-      resolve();
     })
   }
-
-  @ViewChild(MatMenuTrigger) errorTrigger: MatMenuTrigger;
 }
